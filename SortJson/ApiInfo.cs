99440ed565534bf7081fff2977f8e0b00cb9066d
user <user@WIN-F7NIMF7A3VO>
2023-11-21 07:33:27 +0500

    Добавьте файлы проекта.

---
+++ SortJson/ApiInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace SortJson
+{
+    public class ApiInfo
+    {
+        public int count { get; set; }
+        public List<Entry> entries { get; set; }
+    }
+}
---
+++ SortJson/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Приложение начало работу");
+using (var client = new HttpClient())
+{
+    using var result = await client.GetAsync("https://api.publicapis.org/entries");
+    Console.WriteLine(result);
+ }
+ Console.WriteLine("Приложение завершило работу");
